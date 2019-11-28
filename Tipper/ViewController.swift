25e9a400d2197a699854afb444f3dc24492f535e
Argenis De La Rosa <hennyrosexo@gmail.com>
2019-11-28 00:53:04 -0500

    Initial Commit

---
+++ Tipper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipper
+//
+//  Created by argenis delarosa on 11/28/19.
+//  Copyright © 2019 argenis delarosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
